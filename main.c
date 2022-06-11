3b996dfc8a9e732c0f003cc36a0e3539f82965a8
tomoya ishigaki <steinwand.tomo@gmail.com>
2022-06-12 03:04:24 +0900

    Add minimum loop

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int	main(int argc, char** argv) {
+	char	*input;
+
+	while (1) {
+		input = readline("minilisp> ");
+		if (strcmp("exit", input) == 0)
+			break;
+		add_history(input);
+		free(input);
+	}
+	free(input);
+	return (0);
+}
