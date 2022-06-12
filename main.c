d7a1e2e9831019783675c4760d0838c9d4bcb657
tomoya ishigaki <steinwand.tomo@gmail.com>
2022-06-12 20:34:35 +0900

    Add signal handler and logic of adding history

--- main.c
+++ main.c
@@ -1,13 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 
 #include <readline/readline.h>
 #include <readline/history.h>
 
+void	handle_sigint()
+{
+	//printf("\nminishell> ");
+	printf("\n");
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+	return ;
+}
+
+void	handle_sigquit()
+{
+	return ;
+}
+
+void	set_signal_handler(int signo, void *func)
+{
+	struct sigaction	action;
+
+	action.sa_handler = func;
+	if (sigaction(signo, &action, NULL))
+	{
+		perror("sigaction");
+		exit(1);
+	}
+}
+
 int	main(int argc, char** argv) {
 	char	*input;
 
+	set_signal_handler(SIGINT, handle_sigint);
+	set_signal_handler(SIGQUIT, SIG_IGN);
 	while (1) {
 		// readlineを利用して入力を取得
 		input = readline("minishell> ");
@@ -17,7 +47,8 @@ int	main(int argc, char** argv) {
 			printf("exit\n");
 			break ;
 		}
-		add_history(input);
+		if (strcmp(input, "") != 0)
+			add_history(input);
 		free(input);
 	}
 	free(input);
